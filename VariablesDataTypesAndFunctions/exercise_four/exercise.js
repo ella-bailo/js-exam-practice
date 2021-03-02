28ec56d542f7045e116a4fe5560482092a0791d7
Ella Bailo <ella.bailo@zoopla.co.uk>
2021-03-02 19:15:50 +0000

    exersize four completed

---
+++ VariablesDataTypesAndFunctions/exercise_four/exercise.js
@@ -0,0 +1,22 @@
+let randomNumbers = [];
+
+for (i=0; i < 10; i++) {
+randomNumbers.push(Math.random())
+}
+
+randomNumbers.forEach(
+    number => {
+        console.log('\n')
+        console.log(`number: ${number}`)
+        console.log(` number rounded off: ${Math.round(number)}`)
+        console.log(` number rounded up: ${Math.ceil(number)}`)
+        console.log(` number rounded down: ${Math.floor(number)}`)
+        console.log(` positive square root of number : ${Math.sqrt(number)}`)
+        console.log(` number squared: ${Math.pow(number, 2)}`)
+        console.log('\n')
+    }
+)
+
+
+console.log(` Max out of the random numbers ${Math.max(...randomNumbers)}`)
+console.log(` Min out of the random numbers ${Math.min(...randomNumbers)}`)
